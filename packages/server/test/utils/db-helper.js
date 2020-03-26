ed300da6c55fa06a7f86558f8bcb69bcd7e28e00
bcoe <bencoe@google.com>
2020-03-25 22:52:33 -0700

    chore: set the password

--- packages/server/test/utils/db-helper.js
+++ packages/server/test/utils/db-helper.js
@@ -1,13 +1,17 @@
 process.env.DATABASE = 'flaky_dev_test';
 
 const {Client} = require('pg');
-const client = new Client({
+const dbSettings = {
 	user: process.env.POSTGRES_USER || process.env.POSTGRES_USER,
 	port: process.env.POSTGRES_PORT ? Number(process.env.POSTGRES_PORT) : 5432,
 	host: process.env.POSTGRES_HOST || 'localhost',
 	database: process.env.POSTGRES_DB
-});
+};
+if (process.env.POSTGRES_PASSWORD) {
+	dbSettings.password = process.env.POSTGRES_PASSWORD;
+}
 
+const client = new Client(dbSettings);
 const {execSync} = require('child_process');
 
 const output = execSync('npm run migrate:up');
